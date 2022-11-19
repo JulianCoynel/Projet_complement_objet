fcff9efb03918fc5e52c7c7fe6f81c46cec4a199
Skyman28 <107101306+JulianCoynel@users.noreply.github.com>
2022-11-19 13:55:33 +0100

    capteur

--- src/Capteurs/Capteur.java
+++ src/Capteurs/Capteur.java
@@ -1,7 +1,10 @@
 package Capteurs;
 
+import java.util.ArrayList;
 import Segments_de_route.*;
+import Semaphores.*;
 
 public abstract class Capteur {
-	private Segment_route s;
+	private Segment_route son_segment;
+	private ArrayList<Semaphore> ses_semaphores;
 }
