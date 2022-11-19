54178ea2c32c0a5ddb4e4567923d18f806e9cd2e
Skyman28 <107101306+JulianCoynel@users.noreply.github.com>
2022-11-19 14:28:10 +0100

    setter/getter

--- src/Capteurs/Capteur.java
+++ src/Capteurs/Capteur.java
@@ -1,10 +1,41 @@
 package Capteurs;
 
 import java.util.ArrayList;
+
+import Elements_de_regulations.ElementDeRegulation;
 import Segments_de_route.*;
 import Semaphores.*;
 
 public abstract class Capteur {
-	private SegmentDeRoute son_segment;
-	private ArrayList<Semaphore> ses_semaphores;
+	private SegmentDeRoute sonSegment;
+	private ArrayList<Semaphore> sesSemaphores;
+	private ElementDeRegulation sonElement;
+	
+	public void set_sonSegment(SegmentDeRoute s) {
+		sonSegment=s;
+	}
+	
+	public void set_sesSemaphores(ArrayList<? extends Semaphore> sl) {
+		sesSemaphores=new ArrayList<Semaphore>(sl);
+	}
+	
+	public void add_Semaphore(Semaphore s) {
+		sesSemaphores.add(s);
+	}
+	
+	public void set_sonElement(ElementDeRegulation e) {
+		sonElement=e;
+	}
+	
+	public SegmentDeRoute get_sonSegment() {
+		return sonSegment;
+	}
+	
+	public ArrayList<Semaphore> get_sesSemaphores() {
+		return sesSemaphores;
+	}
+	
+	public ElementDeRegulation get_sonElement() {
+		return sonElement;
+	}
 }
