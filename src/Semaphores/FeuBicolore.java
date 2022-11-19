3abf117c3334b0c2bac1a3c6bb2c39c467617d48
MrZapha <73909093+MrZapha@users.noreply.github.com>
2022-11-19 17:28:20 +0100

    ajout javadoc fonction semaphore

--- src/Semaphores/FeuBicolore.java
+++ src/Semaphores/FeuBicolore.java
@@ -5,21 +5,53 @@ import java.util.ArrayList;
 import Capteurs.Capteur;
 import Segments_de_route.SegmentDeRoute;
 
+/**
+ * La classe définissant un sémaphore de type feu bicolore
+ * @author Romain BAUDOUIN
+ *
+ */
 public class FeuBicolore extends Semaphore {
 	
+	/**
+	 * Classe énumérée définissant les deux couleurs possibles d'un feu bicolore
+	 * @author Romain BAUDOUIN
+	 *
+	 */
 	enum Bicolore {ROUGE, VERT}
+	
+	/**
+	 * La couleur du feu
+	 */
 	private Bicolore couleur;
 
+	/**
+	 * Constructeur d'un feu bicolore, on initialise le feu à VERT
+	 * @param segment Son segment de route
+	 * @param capteurs Les capteurs qui lui sont reliés
+	 * @param sens Le sens de circulation auquel il appartient
+	 */
 	public FeuBicolore(SegmentDeRoute segment, ArrayList<Capteur> capteurs, boolean sens) {
 		super(segment, capteurs, sens);
 		this.couleur = Bicolore.VERT;
 	}
 	
+	/**
+	 * Constructeur d'un feu bicolore avec un seul capteur, on initialise le feu à VERT
+	 * @param segment Son segment de route
+	 * @param capteur Un capteur que l'on souhaite relier au sémaphore à sa création
+	 * @param sens Le sens de circulation auquel il appartient
+	 */
 	public FeuBicolore(SegmentDeRoute segment,Capteur capteur,boolean sens) {
 		super(segment, capteur, sens);
 		this.couleur = Bicolore.VERT;
 	}
 	
+	/**
+     * Cette fonction permet de calculer la vitesse à laquelle le véhicule doit se mettre lorsqu'il rencontre le sémaphore
+     * Si le feu est vert on renvoie vitesseActuelle et sinon 0
+     * @param vitesseActuelle la vitesse actuelle du véhicule
+     * @return la nouvelle vitesse qui doit être prise
+     */
 	public int contrainteVitesseSemaphore(int vitesseActuelle) {
 		int res = 0;
 		switch (this.couleur) {
@@ -31,10 +63,18 @@ public class FeuBicolore extends Semaphore {
 		return res;
 	}
 	
+	/**
+	 * Permet de changer la couleur d'un feu bicolore parmi ROUGE et VERT
+	 * @param b la nouvelle couleur du feu
+	 */
 	public void setCouleur(Bicolore b) {
 		couleur = b;
 	}
 	
+	/**
+	 * Permet de récupérer la couleur d'un feu
+	 * @return la couleur actuelle du feu
+	 */
 	public Bicolore getCouleur() {
 		return couleur;
 	}
--- src/Semaphores/Semaphore.java
+++ src/Semaphores/Semaphore.java
@@ -5,44 +5,99 @@ import java.util.ArrayList;
 import Capteurs.Capteur;
 import Segments_de_route.SegmentDeRoute;
 
+/**
+ * La classe abstraite permettant de définir les sémaphores
+ * @author Romain BAUDOUIN
+ *
+ */
 public abstract class Semaphore {
+	/**
+     * Le segment auquel appartient le sémaphore
+     */
 	private SegmentDeRoute sonSegment;
+	/**
+     * La liste de capteurs reliés au sémaphore
+     */
 	private ArrayList<Capteur> sesCapteurs;
+	/**
+     * Le sens de circulation auquel appartient le sémaphore
+     */
 	private boolean sens;
 	
+	/**
+	 * Constructeur d'un sémaphore quelconque
+	 * @param segment Son segment de route
+	 * @param capteurs Les capteurs qui lui sont reliés
+	 * @param sens Le sens de circulation auquel il appartient
+	 */
 	public Semaphore(SegmentDeRoute segment,ArrayList<Capteur> capteurs,boolean sens) {
 		sonSegment = segment;
 		sesCapteurs = capteurs;
 		this.sens = sens;
 	}
 	
+	/**
+	 * Constructeur d'un sémaphore quelconque avec un seul capteur
+	 * @param segment Son segment de route
+	 * @param capteur Un capteur que l'on souhaite relier au sémaphore à sa création
+	 * @param sens Le sens de circulation auquel il appartient
+	 */
 	public Semaphore(SegmentDeRoute segment,Capteur capteur,boolean sens) {
 		this(segment,new ArrayList<Capteur>(),sens);
 		this.add_Capteur(capteur);
 	}
 	
+	/**
+     * Cette fonction permet de calculer la vitesse à laquelle le véhicule doit se mettre lorsqu'il rencontre le sémaphore
+     * @param vitesseActuelle la vitesse actuelle du véhicule
+     * @return la nouvelle vitesse qui doit être prise
+     */
 	public abstract int contrainteVitesseSemaphore(int vitesseActuelle);
 	
+	/**
+	 * Permet de changer le segment de route d'un sémaphore
+	 * @param seg Son segment de route
+	 */
 	public void set_sonSegment(SegmentDeRoute seg) {
 		sonSegment = seg;
 	}
 	
+	/**
+	 * Permet de changer les capteurs d'un sémaphore
+	 * @param cl Une liste de capteurs reliés au sémaphore
+	 */
 	public void set_sesCapteurs(ArrayList<? extends Capteur> cl) {
 		sesCapteurs=new ArrayList<Capteur>(cl);
 	}
 	
+	/**
+	 * Permet d'ajouter un capteur à la liste de capteurs reliés au sémaphore
+	 * @param c Le capteur à ajouter
+	 */
 	public void add_Capteur(Capteur c) {
 		sesCapteurs.add(c);
 	}
 	
+	/**
+	 * Permet de récupérer son segment de route
+	 * @return Son segment de route
+	 */
 	public SegmentDeRoute get_sonSegment() {
 		return sonSegment;
 	}
 	
+	/**
+	 * Permet de récupérer son ArrayList de capteurs
+	 * @return sa liste de capteurs
+	 */
 	public ArrayList<Capteur> get_sesCapteurs() {
 		return sesCapteurs;
 	}
 	
+	/**
+	 * Permet de récupérer le sens de circulation du sémaphore
+	 * @return son sens de circulation
+	 */
 	public boolean get_sonSens() {
 		return sens;
 	}
